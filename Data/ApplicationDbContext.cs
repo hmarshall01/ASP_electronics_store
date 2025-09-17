014e41677298bb91d4a20b8184e3ce82ec60b8c5
hmarshall01 <h.emarshall@outlook.com>
2025-09-17 12:55:14 -0700

    adding files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspTest2._0.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
